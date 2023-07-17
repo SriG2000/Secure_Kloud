bf18917816a7b7c0eb765b19102c4e2eadec514d
Srivatsav Gopinath <120214958+SrivatsavGopinath2000@users.noreply.github.com>
2023-07-16 17:08:55 -0700

    C

--- cmd.sh
+++ cmd.sh
@@ -7,31 +7,6 @@ source venv/bin/activate
 sudo apt install python3 python3-pip
 pip install Flask gunicorn pymysql
 sudo apt install mysql-client-core-8.0   
-
-# MySQL database credentials
-DB_HOST="skdb.csdmbmuvmujj.us-east-2.rds.amazonaws.com"
-DB_USER="srivatsav"
-DB_PASSWORD="srivatsav"
-DB_NAME="skdb"
-
-# SQL code to create the emp table
-SQL_CODE=$(cat << EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-USE $DB_NAME;
-CREATE TABLE IF NOT EXISTS emp (
-    eid INT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
-);
-EOF
-)
-
-# Log in to MySQL and execute the SQL code
-mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD -e "$SQL_CODE"
-
-# Export the Flask environment variables
 export FLASK_APP=app.py
 export FLASK_ENV=development
-
-# Run the Flask application with Gunicorn
 gunicorn app:app -b 0.0.0.0:5000 &
